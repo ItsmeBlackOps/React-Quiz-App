f6d78ccec1a77a895b33c5a8afcdd6dffde94702
ItsmeBlackOps <109592102+ItsmeBlackOps@users.noreply.github.com>
2023-10-25 23:50:55 +0530

    Update Quiz.js

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,44 +1,61 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import DataContext from '../context/dataContext';
+import { useNavigate } from 'react-router-dom'; // Assuming you're using React Router for navigation.
 
-const Quiz = () => {
-    const { showQuiz, question, quizs, checkAnswer, correctAnswer,
-            selectedAnswer,questionIndex, nextQuestion, showTheResult }  = useContext(DataContext);
+const FullNameInput = () => {
+  const { startQuiz } = useContext(DataContext);
+  const [fullName, setFullName] = useState('');
+  const navigate = useNavigate();
 
-    return (
-        <section className="bg-dark text-white" style={{ display: `${showQuiz ? 'block' : 'none'}` }}>
-            <div className="container">
-                <div className="row vh-100 align-items-center justify-content-center">
-                    <div className="col-lg-8">
-                        <div className="card p-4" style={{ background: '#3d3d3d', borderColor: '#646464' }}>
-                            <div className="d-flex justify-content-between gap-md-3">
-                                <h5 className='mb-2 fs-normal lh-base'>{question?.question}</h5>
-                                <h5 style={{ color: '#60d600', width: '100px', textAlign: 'right' }}>{quizs.indexOf(question) + 1} / {quizs?.length}</h5>
-                            </div>
-                            <div>
-                                {
-                                    question?.options?.map((item, index) => <button
-                                        key={index}
-                                        className={`option w-100 text-start btn text-white py-2 px-3 mt-3 rounded btn-dark ${correctAnswer === item && 'bg-success'}`}
-                                        onClick={(event) => checkAnswer(event, item)}
-                                    >
-                                        {item}
-                                    </button>)
-                                }
-                            </div>
+  const handleStartQuiz = async () => {
+    if (fullName.trim() === '') {
+      // Handle empty full name input
+      alert('Please enter your full name.');
+    } else {
+      try {
+        // Make a POST request to the API to save the full name
+        const response = await fetch('https://65393a6ee3b530c8d9e823f5.mockapi.io/Result', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ fullName }),
+        });
 
-                            {
-                                (questionIndex + 1) !== quizs.length ?
-                                    <button className='btn py-2 w-100 mt-3 bg-primary text-light fw-bold' onClick={nextQuestion} disabled={!selectedAnswer}>Next Question</button>
-                                    :
-                                    <button className='btn py-2 w-100 mt-3 bg-primary text-light fw-bold' onClick={showTheResult} disabled={!selectedAnswer}>Show Result</button>
-                            }
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+        if (response.ok) {
+          // If the POST request is successful, start the quiz and redirect
+          startQuiz();
+          navigate.push('/Quiz'); // Navigate to the quiz component
+        } else {
+          // Handle errors, e.g., show an alert
+          alert('Failed to save full name. Please try again.');
+        }
+      } catch (error) {
+        // Handle network errors, e.g., show an alert
+        alert('Network error. Please try again.');
+      }
+    }
+  };
+
+  return (
+    <section className='text-white text-center bg-dark'>
+      <div className="container">
+        <div className="row vh-100 align-items-center justify-content-center">
+          <div className="col-lg-8">
+            <h1 className='fw-bold mb-4'>Enter Your Full Name</h1>
+            <input
+              type="text"
+              placeholder="Full Name"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              className="form-control mb-3"
+            />
+            <button onClick={handleStartQuiz} className="btn px-4 py-2 bg-light text-dark fw-bold">Start Quiz</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
-export default Quiz;
+export default FullNameInput;
